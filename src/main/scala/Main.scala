33056d3b1727933414c854ebe8f02be3bc008259
adamdye12 <adam.dye@qa.com>
2017-06-15 17:28:47 +0000

    Initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,30 @@
+/**
+  * Created by Administrator on 15/06/2017.
+  */
+object Main {
+  def main(args: Array[String]): Unit = println("hello world")
+
+  //language fundamentals
+  var x = 10
+  println(x)
+  x=11
+  println(x)
+  x += 1
+  println(x)
+  x -= 1
+  println(x)
+
+  val y = 10
+  //y=11
+
+  val z:Double = 10
+  println(z)
+  val greeting:String = null
+  val greeting1: Any = "hello"
+  println(greeting)
+  println(greeting1)
+
+  val xmax, ymax = 100
+  val a0=1, var b0=3, val c0= 2.4
+}
+
